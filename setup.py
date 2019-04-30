4c079051d9e747bcdff4254fd44aef21b2a54c41
danheath <madowl@WINDOWS-T2K99N2.localdomain>
2019-04-29 23:15:12 -0400

    updated and added files for pypi release

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pykollib",
+    version="0.9.1",
+    author="",
+    author_email="dan@heathmailbox.com",
+    description="Python library for interacting with the Kingdom of Loathing ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/danheath/pykollib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
