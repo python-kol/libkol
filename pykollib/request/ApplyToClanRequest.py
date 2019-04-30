3a1e1bf00ba65500f4c0ff0a0e7ecc3125e04e6f
Samuel Gaus <sam@gaus.co.uk>
2019-04-30 17:22:08 +0100

    Add Clan class and some additional class request types

---
+++ pykollib/request/ApplyToClanRequest.py
@@ -0,0 +1,30 @@
+from .GenericRequest import GenericRequest
+
+class ApplyToClanRequest(GenericRequest):
+    def __init__(self, session, target_id):
+        super(ApplyToClanRequest, self).__init__(session)
+        self.url = session.serverURL + "showclan.php"
+
+        self.requestData = {
+            "recruiter": 1,
+            "pwd": session.pwd,
+            "whichclan": target_id,
+            "action": "joinclan",
+            "apply": "Apply+to+this+Clan",
+            "confirm": "on"
+        }
+
+    def parseResponse(self):
+        """
+        Returns a dict with the following possible elements:
+            accepted: boolean
+            alreadyMember: boolean
+        """
+
+        accepted = self.searchNamedPattern("clanApplicationAccepted")
+        alreadyMember = self.searchNamedPattern("clanApplicationAlreadyMember")
+
+        self.response = {
+            "success": accepted or alreadyMember,
+            "alreadyMember": alreadyMember,
+        }
