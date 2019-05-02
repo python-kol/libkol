0b781542a61a5addea8082029402602855ea23df
Samuel Gaus <sam@gaus.co.uk>
2019-05-02 19:40:39 +0100

    Missing pickledb from setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/danheath/pykollib",
     packages=setuptools.find_packages(),
-    install_requires=["requests"],
+    install_requires=["requests", "pickledb"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
