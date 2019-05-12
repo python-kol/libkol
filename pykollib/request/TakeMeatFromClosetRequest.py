21e2d155269b005efc1d46f8190521af7103cc32
danheath <madowl@WINDOWS-T2K99N2.localdomain>
2019-05-12 18:16:13 -0400

    This time removing the old class

--- pykollib/request/TakeMeatFromClosetRequest.py
+++ pykollib/request/TakeMeatFromClosetRequest.py
@@ -4,16 +4,6 @@ if TYPE_CHECKING:
     from ..Session import Session
 
 
-class TakeMeatFromClosetRequest(GenericRequest):
-    "Adds meat to the player's closet."
-
-    def __init__(self, session, meat=""):
-        super(TakeMeatFromClosetRequest, self).__init__(session)
-        self.url = session.server_url + "closet.php"
-        self.requestData["pwd"] = session.pwd
-        self.requestData["action"] = "takemeat"
-        self.requestData["amt"] = meat
-
 def takeMeatFromClosetRequest(session: "Session", amt: "Amt" = 0) -> ClientResponse:
 
     "Adds meat to the player's closet."
