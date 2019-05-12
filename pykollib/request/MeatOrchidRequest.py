ac0c26d1efd0a9d3303100729352a150b29bca8c
danheath <madowl@WINDOWS-T2K99N2.localdomain>
2019-05-12 19:05:49 -0400

    added all without parsing for initial conversion

--- pykollib/request/MeatOrchidRequest.py
+++ pykollib/request/MeatOrchidRequest.py
@@ -1,15 +1,12 @@
-from .GenericRequest import GenericRequest
-from pykollib.util import ParseResponseUtils
+from aiohttp import ClientResponse
 
 
-class MeatOrchidRequest(GenericRequest):
-    "Visits the hanging meat orchid in the clan rumpus room."
+def meatOrchidRequest(session: "Session") -> ClientResponse:
+    "Uses the hanging meat orchid in the clan rumpus room."
 
-    def __init__(self, session):
-        super(MeatOrchidRequest, self).__init__(session)
-        self.url = session.server_url + "clan_rumpus.php?action=click&spot=1&furni=4"
+    params["action"] = "click"
+    params["spot"] = 1
+    params["furni] = 4
 
-    def parseResponse(self):
-        self.responseData["meat"] = ParseResponseUtils.parseMeatGainedLost(
-            self.responseText
-        )
+    return session.request("clan_rumpus.php", params=params)
+
