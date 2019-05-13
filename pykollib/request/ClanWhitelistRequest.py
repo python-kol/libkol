a6b7d047656511ca54653c4cca31b2a99c5188b1
Samuel Gaus <sam@gaus.co.uk>
2019-05-13 21:45:17 +0100

    Move over various clan whitelist requests

--- pykollib/request/ClanWhitelistRequest.py
+++ pykollib/request/ClanWhitelistRequest.py
@@ -1,61 +1,72 @@
-from .GenericRequest import GenericRequest
-from pykollib.pattern import PatternManager
+import re
+from bs4 import BeautifulSoup
+from aiohttp import ClientResponse
+from typing import TYPE_CHECKING
 
 
-class ClanWhitelistRequest(GenericRequest):
+if TYPE_CHECKING:
+    from ..Session import Session
+
+rankNameAndNumber = re.compile(r"(.*?) \(°([0-9]+)\)")
+
+
+def parse(html: str, include_rank: bool = False, **kwargs):
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Get rid of stupid forms everywhere
+    for f in soup.find_all("form"):
+        f.unwrap()
+
+    # If we want to include the ranks of the whitelist we can, but it's off by default
+    if include_rank:
+        ranks = [
+            {"id": id, "name": name, "number": number}
+            for id, name, number in (
+                (int(o["value"]), *rankNameAndNumber.match(o.string).groups())
+                for o in soup.find(
+                    "select", attrs={"name": lambda n: n and n.startswith("level")}
+                ).children
+            )
+        ]
+
+    members = [
+        {
+            "username": cells[0].a.b.string,
+            "user_id": cells[0].input["value"],
+            "rank": next(
+                (
+                    rank
+                    for rank in ranks
+                    if (
+                        rank["id"]
+                        == int(
+                            cells[1]
+                            .find("select")
+                            .find("option", selected=True)["value"]
+                        )
+                        if cells[1].find("select")
+                        else rank["name"] == cells[1].string
+                    )
+                ),
+                None,
+            )
+            if include_rank
+            else None,
+        }
+        for cells in (
+            row.find_all("td")
+            for table in [
+                soup.find("b", text="People In Your Clan"),
+                soup.find("b", text="People Not In Your Clan"),
+            ]
+            for row in table.find_parent("tr").find_next_siblings("tr")[1:]
+        )
+    ]
+
+    return members
+
+
+def clanWhitelistRequest(session: "Session") -> ClientResponse:
     "Retrieves information from the clan whitelist page."
 
-    def __init__(self, session):
-        super(ClanWhitelistRequest, self).__init__(session)
-        self.url = session.server_url + "clan_whitelist.php"
-
-    def parseResponse(self):
-        # Get the set of clan ranks.
-        ranks = []
-        ranksById = {}
-        rankContainerPattern = PatternManager.getOrCompilePattern("clanRankContainer")
-        match = rankContainerPattern.search(self.responseText)
-        if match:
-            rankText = match.group(1)
-            rankPattern = PatternManager.getOrCompilePattern("clanRank")
-            for rankMatch in rankPattern.finditer(rankText):
-                rank = {}
-                rank["rankId"] = int(rankMatch.group(1))
-                rank["rankName"] = rankMatch.group(2)
-                rank["rankNumber"] = int(rankMatch.group(3))
-                ranks.append(rank)
-                ranksById[rank["rankId"]] = rank
-
-        # Get a list of users who are whitelisted to the clan.
-        members = []
-        memberPattern = PatternManager.getOrCompilePattern("clanWhitelistMember")
-        for match in memberPattern.finditer(self.responseText):
-            member = {}
-            member["userId"] = match.group("userId")
-            member["userName"] = match.group("userName")
-            member["clanTitle"] = match.group("clanTitle")
-            rankId = match.group("clanRankId")
-            rankName = match.group("clanRankName")
-            rankNumber = None
-            if rankId != None:
-                rank = ranksById[int(rankId)]
-                member["rankId"] = rank["rankId"]
-                member["rankName"] = rank["rankName"]
-                member["rankNumber"] = rank["rankNumber"]
-            elif rankName != None:
-                member["rankName"] = rankName
-                foundRank = False
-                for rank in ranks:
-                    if rank["rankName"] == rankName:
-                        foundRank = True
-                        break
-                if foundRank == False:
-                    rank = {}
-                    rank["rankId"] = -1
-                    rank["rankName"] = rankName
-                    rank["rankNumber"] = -1
-                    ranks.append(rank)
-            members.append(member)
-
-        self.responseData["ranks"] = ranks
-        self.responseData["members"] = members
+    return session.request("clan_whitelist.php", parse=parse)
