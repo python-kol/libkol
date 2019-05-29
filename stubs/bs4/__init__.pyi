1177d65f7cc0975ede03f60f41256eb1fa43cbb0
Samuel Gaus <sam@gaus.co.uk>
2019-05-29 21:54:26 +0100

    Further configure mypy

---
+++ stubs/bs4/__init__.pyi
@@ -0,0 +1,13 @@
+from typing import Optional
+from .element import (
+    Tag as Tag,
+    SoupStrainer as SoupStrainer
+)
+
+class BeautifulSoup(Tag):
+    def __init__(
+        self,
+        markup: str = ...,
+        features: Optional[str] = ...,
+        parse_only: Optional[SoupStrainer] = ...,
+) -> None: ...
