0c7b8cdc567fdfa81855c0ca67f7951b0163d878
Samuel Gaus <sam@gaus.co.uk>
2019-05-31 17:35:55 +0100

    Update API layer to match new requests

--- pykollib/Clan.py
+++ pykollib/Clan.py
@@ -17,6 +17,8 @@ from .request import (
     clan_whitelist_remove,
 )
 
+from .request.clan_raids_previous import Raid
+
 
 class Clan(object):
     "This class represents a Clan in the Kingdom of Loathing"
@@ -48,58 +50,62 @@ class Clan(object):
 
     @logged_in
     async def get_raids(self):
-        return await self.session.parse(clan_raids)
+        return await clan_raids(self.session).parse()
 
     @logged_in
     async def get_raid_log(self, raid_id: int):
-        return await self.session.parse(clan_raid_log, raid_id)
+        return await clan_raid_log(self.session, raid_id).parse()
 
     @logged_in
     async def get_stash(self):
-        return await self.session.parse(clan_stash)
+        return await clan_stash(self.session).parse()
 
     @logged_in
     async def add_user_to_whitelist(self, user, rank: int = 0, title: str = "") -> bool:
-        return await self.session.parse(clan_whitelist_add, user, rank, title)
+        return (await clan_whitelist_add(self.session, user, rank, title).parse()).success
 
     @logged_in
     async def remove_user_from_whitelist(self, user) -> bool:
-        return await self.session.parse(clan_whitelist_remove, user)
+        return await clan_whitelist_remove(self.session, user).parse()
 
     @logged_in
     async def get_whitelist(self, include_rank: bool = False) -> List[Dict[str, Any]]:
-        return await self.session.parse(clan_whitelist)
+        return await clan_whitelist(self.session).parse()
 
     @logged_in
     async def get_rank_permissions(self) -> List[Dict[str, Any]]:
-        return await self.session.parse(clan_ranks)
+        return await clan_ranks(self.session).parse()
 
     @logged_in
     async def get_ranks(self) -> List[Dict[str, Any]]:
-        return await self.session.parse(clan_whitelist, parse_args={"only_rank": True})
+        return await clan_whitelist(self.session).parse(only_rank=True)
 
     @logged_in
-    async def get_previous_raids(self, limit: int = None) -> List[Dict[str, Any]]:
+    async def get_previous_raids(self, limit: int = None) -> List[Raid]:
         s = self.session
 
-        raids = []
+        raids = [] # type: List[Raid]
 
         if limit is None:
             first_page = await s.parse(clan_raids_previous, page=0)
             raids += first_page["raids"]
             limit = first_page["total"]
 
+        requests = []
         tasks = []
         for page in range(ceil(limit / 10)):
             if page == 0 and len(raids) > 0:
                 continue
-            task = asyncio.ensure_future(clan_raids_previous(s, page=page))
+
+            request = clan_raids_previous(s, page=page)
+            task = asyncio.ensure_future(request.request)
+
+            requests.append(request)
             tasks.append(task)
 
-        r = await asyncio.gather(*tasks)
-        for page in r:
-            data = await page.parse()
-            raids += data["raids"]
+        await asyncio.gather(*tasks)
+        for request in requests:
+            raids += (await request.parse()).raids
 
         return raids
 
--- pykollib/Skill.py
+++ pykollib/Skill.py
@@ -1,5 +1,10 @@
-from typing import NamedTuple
-
-
-class Skill(NamedTuple):
+class Skill():
     id: int
+    buff: bool
+
+    def __init__(self, id: int, buff: bool = False):
+        self.id = id
+        self.buff = buff
+
+    def __getitem__(self, key: int) -> "Skill":
+        return Skill(id=key)
