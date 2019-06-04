d4a5ebd0b807385d3bf4e36477590a3add973bcf
Samuel Gaus <sam@gaus.co.uk>
2019-06-04 22:49:23 +0100

    Add trophy buy request

---
+++ pykollib/request/trophy_buy.py
@@ -0,0 +1,22 @@
+import pykollib
+
+from ..Error import InvalidActionError, UnknownError
+from ..Trophy import Trophy
+from .request import Request
+
+
+class trophy_buy(Request):
+    def __init__(self, session: "pykollib.Session", trophy: Trophy) -> None:
+        super().__init__(session)
+        data = {"action": "buytrophy", "whichtrophy": trophy.id}
+        self.request = session.request("trophy.php", data=data)
+
+    @staticmethod
+    async def parser(content: str, **kwargs) -> bool:
+        if "<td>You don't meet the requirements for that trophy.</td>" in content:
+            raise InvalidActionError("Cannot get that trophy")
+
+        if "<td>Your trophy has been installed at your campsite.</td>" not in content:
+            raise UnknownError("Unknown error buying trophy")
+
+        return True
