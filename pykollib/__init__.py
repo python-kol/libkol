cd3d28d932ee1ddf5ccfb8462fa2e607bbb6a130
Samuel Gaus <sam@gaus.co.uk>
2019-06-05 20:08:31 +0100

    Added a `run` utility function to let people set up pykollib without importing asyncio

--- pykollib/__init__.py
+++ pykollib/__init__.py
@@ -1,3 +1,6 @@
+import asyncio
+from typing import Callable
+
 from .Clan import Clan
 from .Error import Error
 from .FoldGroup import FoldGroup
@@ -8,5 +11,10 @@ from .Session import Session
 from .ZapGroup import ZapGroup
 from .Store import Store
 from .Trophy import Trophy
+
+def run(func: Callable):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(func())
+    loop.close()
 
 __all__ = ["Clan", "Error", "Session", "Kmail", "Item", "types", "FoldGroup", "ZapGroup", "Store", "Trophy"]
