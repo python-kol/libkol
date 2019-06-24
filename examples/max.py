976be723e1678d10d0b2e1e98fbe51bd423f22d6
Samuel Gaus <sam@gaus.co.uk>
2019-06-24 19:14:30 +0100

    Show better maximization example

--- examples/max.py
+++ examples/max.py
@@ -1,16 +1,20 @@
-from libkol import Session, run, Modifier, Maximizer
+from libkol import Session, run, Modifier, Maximizer, Item
 
 
 async def main():
     async with Session() as kol:
         problem = Maximizer(kol)
 
-        problem += Modifier.Moxie
-        problem -= Modifier.CombatRate * 5
-        items = await problem.solve()
+        problem += Modifier.HotResistance
+        problem += await Item["high-temperature mining drill"]
 
-        for i in items:
-            print(f"{i.type}: {i.name}")
+        try:
+            items = await problem.solve()
+
+            for i in items:
+                print(f"{i.type}: {i.name}")
+        except ValueError as e:
+            print(f"Not possible ({e})")
 
 
 if __name__ == "__main__":
