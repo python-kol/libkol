f2eea2d66b4ce30999ad58865067502cf26ab73f
Samuel Gaus <sam@gaus.co.uk>
2019-06-24 23:48:18 +0100

    Update equipment tests cause we now return a Dict

--- test/request/test_equipment.py
+++ test/request/test_equipment.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+from libkol import Slot
 from libkol.request import equipment
 
 from .test_base import TestCase
@@ -12,32 +13,34 @@ class EquipmentTestCase(TestCase):
         async def run_test(file):
             outfit = await equipment.parser(file.read(), session=self.session)
 
-            self.assertEqual(outfit.hat.id, 2078)
-            self.assertEqual(outfit.back.id, 5738)
-            self.assertEqual(outfit.shirt.id, 3837)
-            self.assertEqual(outfit.weapon.id, 9893)
-            self.assertEqual(outfit.offhand, None)
-            self.assertEqual(outfit.pants.id, 9406)
+            self.assertEqual(outfit[Slot.Hat].id, 2078)
+            self.assertEqual(outfit[Slot.Back].id, 5738)
+            self.assertEqual(outfit[Slot.Shirt].id, 3837)
+            self.assertEqual(outfit[Slot.Weapon].id, 9893)
+            self.assertEqual(outfit[Slot.Offhand], None)
+            self.assertEqual(outfit[Slot.Pants].id, 9406)
             self.assertEqual(
-                Counter([outfit.acc1.id, outfit.acc2.id, outfit.acc3.id]),
+                Counter(
+                    [outfit[Slot.Acc1].id, outfit[Slot.Acc2].id, outfit[Slot.Acc3].id]
+                ),
                 Counter([6955, 3322, 6956]),
             )
-            self.assertEqual(outfit.familiar.id, 4135)
+            self.assertEqual(outfit[Slot.FamiliarEquipment].id, 4135)
 
         self.run_async("accessories_merged", run_test)
 
     def test_equipment_accessories_separate(self):
         async def run_test(file):
-            outfit = await equipment.parser(file.read())
-            self.assertEqual(outfit.hat.id, 2078)
-            self.assertEqual(outfit.back.id, 5738)
-            self.assertEqual(outfit.shirt.id, 3837)
-            self.assertEqual(outfit.weapon.id, 9893)
-            self.assertEqual(outfit.offhand, None)
-            self.assertEqual(outfit.pants.id, 9406)
-            self.assertEqual(outfit.acc1.id, 6955)
-            self.assertEqual(outfit.acc2.id, 3322)
-            self.assertEqual(outfit.acc3.id, 6956)
-            self.assertEqual(outfit.familiar.id, 4135)
+            outfit = await equipment.parser(file.read(), session=self.session)
+            self.assertEqual(outfit[Slot.Hat].id, 2078)
+            self.assertEqual(outfit[Slot.Back].id, 5738)
+            self.assertEqual(outfit[Slot.Shirt].id, 3837)
+            self.assertEqual(outfit[Slot.Weapon].id, 9893)
+            self.assertEqual(outfit[Slot.Offhand], None)
+            self.assertEqual(outfit[Slot.Pants].id, 9406)
+            self.assertEqual(outfit[Slot.Acc1].id, 6955)
+            self.assertEqual(outfit[Slot.Acc2].id, 3322)
+            self.assertEqual(outfit[Slot.Acc3].id, 6956)
+            self.assertEqual(outfit[Slot.FamiliarEquipment].id, 4135)
 
         self.run_async("accessories_separate", run_test)
