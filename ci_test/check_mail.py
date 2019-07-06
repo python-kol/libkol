586ecb9125e0b1420c348c9705289cd080241b44
danheath <dan@heathmailbox.com>
2019-07-05 20:37:10 -0400

    trying to get a ci test working

--- ci_test/check_mail.py
+++ ci_test/check_mail.py
@@ -5,7 +5,7 @@
 
 import asyncio
 from libkol import Session
-from libkol.request import getChatMessagesRequest, openChatRequest
+from libkol.request import kmail_get
 import configparser
 import os
 
@@ -15,15 +15,19 @@ config.read(os.path.join(os.getenv("HOME"), 'libkolconfig'))
 
 async def main():
     async with Session() as kol:
+        print("Logging In")
         await kol.login(config['DEFAULT']['username'], config['DEFAULT']['password'])
 
-        kmails = await kol.kmail.get()
-        for kmail in kmails:
-            print("Received kmail from %s (#%s)" % (kmail["userName"], kmail["userId"]))
-            print("Text: %s" % kmail["text"])
-            print("Meat: %s" % kmail["meat"])
-            for item in kmail["items"]:
-                print("Item: %s (%s)" % (item["name"], item["quantity"]))
+        print("Getting kmail")
+#        kmails = await kol.kmail.get()
+#        print(kmails)
+#        print("Parsing kmail")
+#        for kmail in kmails:
+#            print("Received kmail from %s (#%s)" % (kmail["userName"], kmail["userId"]))
+#            print("Text: %s" % kmail["text"])
+#            print("Meat: %s" % kmail["meat"])
+#            for item in kmail["items"]:
+#                print("Item: %s (%s)" % (item["name"], item["quantity"]))
 
 
 if __name__ == "__main__":
