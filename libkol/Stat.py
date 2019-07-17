9846fce19bb5c6166d968a43129af34dd6c8c0f1
Samuel Gaus <sam@gaus.co.uk>
2019-07-17 22:31:38 +0100

    Stat can return substat names

--- libkol/Stat.py
+++ libkol/Stat.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import List
 
 from .CharacterClass import CharacterClass
 
@@ -10,3 +11,20 @@ class Stat(Enum):
 
     def __contains__(self, character_class: CharacterClass) -> bool:
         return character_class.stat == self
+
+    @property
+    def substats(self) -> List[str]:
+        if self is self.Muscle:
+            return [
+                "Beefiness",
+                "Fortitude",
+                "Muscleboundness",
+                "Strengthliness",
+                "Strongness",
+            ]
+        if self is self.Mysticality:
+            return ["Enchantedness", "Magicalness", "Mysteriousness", "Wizardliness"]
+        if self is self.Moxie:
+            return ["Cheek", "Chutzpah", "Roguishness", "Sarcasm", "Smarm"]
+
+        return []
